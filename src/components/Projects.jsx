619263ff8075cb3601512491abc3b94587baa707
TSENE <marilynedelia@gmail.com>
2024-08-03 11:14:11 +0200

    ajout des liens de page descritives

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,6 +8,7 @@ import Image from 'next/image';
 
 import db from '../api/db.json'
 import Link from 'next/link';
+import { ChevronRight, SquareArrowOutUpRight } from 'lucide-react';
 
 
 const Projects = () => {
@@ -22,8 +23,8 @@ const Projects = () => {
       </h2>
       <div className=" pt-20 flex flex-col gap-32">
         {db.projects.map((project, index) => (
-          <div className={`flex flex-col items-center justify-center gap-8 md:gap-24 ${index % 2 !== 0 ? 'md:flex-row-reverse' : 'md:flex-row'}`} key={index}>
-            <div className={`relative w-full h-full md:h-auto lg:w-3/4 flex flex-col md:flex-row gap-6 md:gap-14 ${index % 2 !== 0 ? 'md:flex-row-reverse' : 'md:flex-row'}`} >
+          <div className={`  flex flex-col  gap-2 md:gap-24 md:items-center md:justify-center${index % 2 !== 0 ? 'md:flex-row-reverse' : 'md:flex-row'}`} key={index}>
+            <div className={` shadow-md md:shadow-none relative w-full h-full md:h-auto lg:w-3/4 flex flex-col md:flex-row gap-6 md:gap-14 ${index % 2 !== 0 ? 'md:flex-row-reverse' : 'md:flex-row'}`} >
               <Link href={`project/${project.id}`} className="absolute inset-0 object-cover h-full w-full md:w-1/2 md:relative ">
                 <Image
                     src={getImagePath(project.image)}
@@ -33,6 +34,10 @@ const Projects = () => {
                     height={430}
 
                   />
+                  <div className={`text-green curtain absolute inset-0 bg-background bg-opacity-70 gap-1 flex justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300 ${supreme.className}`}>
+                    <p className=' text-lg'>En savoir plus</p>
+                    <SquareArrowOutUpRight size={18}/>
+                  </div>
               </Link>
             
               <div className="relative z-10 flex flex-col gap-6 bg-background  bg-opacity-90 md:bg-none md:relative md:bg-transparent p-4 md:p-0 md:w-1/2">
@@ -55,7 +60,9 @@ const Projects = () => {
                   </a>
                 </div>
               </div>
+          
             </div>
+            <Link href={`project/${project.id}`} className={`hover:text-green md:hidden duration-300 flex gap-1 text-sm items-center ${supreme.className}`}>En savoir plus <ChevronRight /></Link>
           </div>
         ))}
       </div>
